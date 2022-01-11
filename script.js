babf96ac43c9f8fba4b2b1ba0a7a74bbfad4c2aa
eriksvetlik <svetlik.erik@gmail.com>
2022-01-11 02:06:30 -0600

    added navbar and APIs

---
+++ script.js
@@ -0,0 +1,55 @@
+var apiKEY = "fba9ebceabe00d55b72dc111e8106b60";
+var city = "london";
+var requestUrl;
+
+function getCityWeather(city) {
+  currentAPI();
+
+  function currentAPI() {
+    requestUrl =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&appid=" +
+      apiKEY +
+      "&units=imperial";
+
+    fetch(requestUrl)
+      .then(function (response) {
+        if (response.status === 404) {
+          console.log(response.status);
+          return;
+        }
+        return response.json();
+      })
+      .then(function (data) {
+        var lat = data.coord.lat;
+        var lon = data.coord.lon;
+        oneCallAPI(lat, lon);
+      });
+  }
+
+  function oneCallAPI(lat, lon) {
+    requestUrl =
+      "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&exclude=minutely,hourly,alerts&appid=" +
+      apiKEY +
+      "&units=imperial";
+
+    fetch(requestUrl)
+      .then(function (response) {
+        if (response.status === 404) {
+          console.log(response.status);
+          return;
+        }
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+      });
+  }
+}
+
+getCityWeather();
