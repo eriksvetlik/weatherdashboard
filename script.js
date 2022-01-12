93321880944f7d4fe76325650dac568d194f95da
eriksvetlik <svetlik.erik@gmail.com>
2022-01-11 20:46:54 -0600

    added functionality to add data from APIs and display in index.html

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 var apiKEY = "c0955b95b1b4f37d644a512418fc63bc";
 // var apiKEY = "fba9ebceabe00d55b72dc111e8106b60";
-var city = "london";
+var city = "houston";
+var current = document.getElementById("current");
+var future = document.getElementById("future");
 var requestUrl;
 
-function getCityWeather(city) {
+function getCityWeather() {
+  // e.preventDefault();
+
   currentAPI();
 
   function currentAPI() {
@@ -24,6 +28,39 @@ function getCityWeather(city) {
       })
       .then(function (data) {
         console.log(data);
+
+        // name
+        var name = document.createElement("h3");
+        name.textContent = data.name;
+        current.append(name);
+
+        // icon
+        var icon = data.weather[0].icon;
+        var img = document.createElement("img");
+        img.src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+        current.append(img);
+
+        // current day
+        var currentDay = document.createElement("h6");
+        currentDay.textContent = moment().format("MMMM D, YYYY");
+        current.append(currentDay);
+
+        // temperature
+        var temperature = document.createElement("h4");
+        temperature.textContent = data.main.temp + "\xB0F";
+        current.append(temperature);
+
+        // humidity
+        var humidity = document.createElement("h5");
+        humidity.textContent = "Humidity: " + data.main.humidity;
+        current.append(humidity);
+
+        // wind speed
+        var windSpeed = document.createElement("h5");
+        windSpeed.textContent = "Wind Speed: " + data.wind.speed + "mph";
+        current.append(windSpeed);
+
+        // capture lat and lon values for the oneCallAPI function
         var lat = data.coord.lat;
         var lon = data.coord.lon;
         oneCallAPI(lat, lon);
@@ -50,8 +87,13 @@ function getCityWeather(city) {
       })
       .then(function (data) {
         console.log(data);
+
+        // uv index
+        var uvIndex = document.createElement("h5");
+        uvIndex.textContent = "UV Index: " + data.current.uvi;
+        current.append(uvIndex);
       });
   }
 }
 
-getCityWeather(city);
+getCityWeather();
