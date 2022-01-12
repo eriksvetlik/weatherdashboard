0ac9678d9a3e31ce8e85a565c880b6823d1aec5a
eriksvetlik <svetlik.erik@gmail.com>
2022-01-12 10:14:27 -0600

    added future day values

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var apiKEY = "c0955b95b1b4f37d644a512418fc63bc";
-// var apiKEY = "fba9ebceabe00d55b72dc111e8106b60";
+// var apiKEY = "c0955b95b1b4f37d644a512418fc63bc";
+var apiKEY = "fba9ebceabe00d55b72dc111e8106b60";
 var city = "houston";
 var current = document.getElementById("current");
 var future = document.getElementById("future");
@@ -57,7 +57,7 @@ function getCityWeather() {
 
         // wind speed
         var windSpeed = document.createElement("h5");
-        windSpeed.textContent = "Wind Speed: " + data.wind.speed + "mph";
+        windSpeed.textContent = "Wind Speed: " + data.wind.speed + " mph";
         current.append(windSpeed);
 
         // capture lat and lon values for the oneCallAPI function
@@ -88,10 +88,45 @@ function getCityWeather() {
       .then(function (data) {
         console.log(data);
 
-        // uv index
+        // uv index for current
         var uvIndex = document.createElement("h5");
         uvIndex.textContent = "UV Index: " + data.current.uvi;
         current.append(uvIndex);
+
+        for (var i = 0; i < 5; i++) {
+          // create div container
+          var fdiv = document.createElement("div");
+          future.append(fdiv);
+
+          // icon
+          var ficon = data.daily[i].weather[0].icon;
+          var fimg = document.createElement("img");
+          fimg.src = "https://openweathermap.org/img/wn/" + ficon + "@2x.png";
+          fdiv.append(fimg);
+
+          // future day
+          var futureDay = document.createElement("h6");
+          futureDay.textContent = moment()
+            .add(i + 1, "day")
+            .format("MMMM D, YYYY");
+          fdiv.append(futureDay);
+
+          // temperature
+          var ftemperature = document.createElement("h4");
+          ftemperature.textContent = data.daily[i].temp.day + "\xB0F";
+          fdiv.append(ftemperature);
+
+          // humidity
+          var fhumidity = document.createElement("h5");
+          fhumidity.textContent = "Humidity: " + data.daily[i].humidity;
+          fdiv.append(fhumidity);
+
+          // wind speed
+          var fwindSpeed = document.createElement("h5");
+          fwindSpeed.textContent =
+            "Wind Speed: " + data.daily[i].wind_speed + " mph";
+          fdiv.append(fwindSpeed);
+        }
       });
   }
 }
